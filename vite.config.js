2b6f5fe49235c5d3e1092f0518059ea6851242ca
omaru <omarux@gmail.com>
2022-02-21 20:37:05 +0100

    missing path name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://omaru.github.io/',
+  base:'https://omaru.github.io/gh-react',
   plugins: [react()]
 })
